3f866c147f907099facb723f6d4f75597c20f55f
tutunamayanlar2021 <kaderoral2147@gmail.com>
2023-07-15 01:20:09 +0300

    Initial Commit

---
+++ Zootopia/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Zootopia
+//
+//  Created by Kader Oral on 15.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
